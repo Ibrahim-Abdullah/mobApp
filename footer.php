96067cbe51e66b6be31efef9b5764c91323078cc
Ibrahim-Abdullah <braimahabdulla@gmail.com>
2017-10-12 22:58:45 -0800

    all file deleted

--- footer.php
+++
@@ -1,9 +0,0 @@
-        <footer class="black page-footer">
-          <div class="container">
-          </div>
-          <div class="footer-copyright row">
-            <div class="container col s12">
-             Ashesi University College © <?php echo date("Y"); ?>
-            </div>
-          </div>
-        </footer>
